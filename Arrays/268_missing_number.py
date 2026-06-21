6b964ae44808320e747ec517ae612f696ef60625
syAnasali <anas67660@gmail.com>
2026-06-21 14:16:14 +0530

    21-06-2026

---
+++ Arrays/268_missing_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i, v in enumerate(nums):
+            if (i!=v):
+                return v-1
+            if v==len(nums)-1:
+                return v+1
---
+++ Arrays/448_find_all_numbers_disappeared_in_an_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        set_nums = set(nums)
+        ret = []
+        for i in range(1, len(nums)+1):
+            if i not in set_nums:
+                ret.append(i)
+        return ret
